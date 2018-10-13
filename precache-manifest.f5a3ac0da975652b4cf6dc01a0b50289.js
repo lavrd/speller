cbd246bcd3890e1bc83fb22f8d609300a5db92e9
spacelavr <spacelavr@gmail.com>
2018-10-13 12:54:11 +0300

    Updates

---
+++ precache-manifest.f5a3ac0da975652b4cf6dc01a0b50289.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48e546446dae029e8364",
+    "url": "/speller/static/js/runtime~main.48e54644.js"
+  },
+  {
+    "revision": "b9cd242e1394007cf806",
+    "url": "/speller/static/js/main.b9cd242e.chunk.js"
+  },
+  {
+    "revision": "d0ddaf3c4410cf1d9a6b",
+    "url": "/speller/static/js/1.d0ddaf3c.chunk.js"
+  },
+  {
+    "revision": "b9cd242e1394007cf806",
+    "url": "/speller/static/css/main.6f1d3832.chunk.css"
+  },
+  {
+    "revision": "50a54d794c08c5ac0d829e5cadf533f6",
+    "url": "/speller/index.html"
+  }
+];
